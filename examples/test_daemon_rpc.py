12884f0ff8e87c3cfc787d339cae0c9dd69bf4ae
Norman Moeschter <norman.moeschter@gmail.com>
2018-09-26 22:03:35 +0200

    Added examples.

---
+++ examples/test_daemon_rpc.py
@@ -0,0 +1,12 @@
+from monerorpc.authproxy import AuthServiceProxy, JSONRPCException
+import logging
+logging.basicConfig()
+logging.getLogger("MoneroRPC").setLevel(logging.DEBUG)
+log = logging.getLogger("wallet-rpc-lib")
+
+rpc = AuthServiceProxy('http://test:test@127.0.0.1:18081/json_rpc')
+#rpc = AuthServiceProxy('http://127.0.0.1:18081/json_rpc')
+try:
+  rpc.get_info()
+except (JSONRPCException) as e:
+  log.error(e)
