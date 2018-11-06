9c9dc51da01c784f4c7d10d291251978ec778404
Norman Moeschter <norman.moeschter@gmail.com>
2018-11-06 06:13:58 +0100

    Added LICENSE.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='python-monerorpc',
-      version='0.5.2',
+      version='0.5.3',
       description='Enhanced version of python-jsonrpc for Monero (monerod, monero-wallet-rpc).',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
@@ -12,9 +12,9 @@ setup(name='python-monerorpc',
       maintainer='Norman Moeschter-Schenck',
       maintainer_email='<norman.moeschter@gmail.com>',
       url='https://www.github.com/monero-ecosystem/python-monerorpc',
-      download_url='https://github.com/monero-ecosystem/python-monerorpc/archive/0.5.2.tar.gz',
+      download_url='https://github.com/monero-ecosystem/python-monerorpc/archive/0.5.3.tar.gz',
       packages=['monerorpc'],
       install_requires=[
           'requests',
       ],
-      classifiers=['License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)', 'Operating System :: OS Independent'])
+      classifiers=['License :: OSI Approved :: MIT License', 'Operating System :: OS Independent'])
