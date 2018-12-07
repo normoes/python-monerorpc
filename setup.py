56966766fb630a01f42adc6762f40dd5718e966c
Norman Moeschter <norman.moeschter@gmail.com>
2018-12-07 14:27:48 +0100

    Updated PyPi setup.py.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='python-monerorpc',
-    version='0.5.3',
+    version='0.5.4',
     description='Enhanced version of python-jsonrpc for Monero (monerod, monero-wallet-rpc).',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -13,7 +13,7 @@ setup(
     maintainer='Norman Moeschter-Schenck',
     maintainer_email='<norman.moeschter@gmail.com>',
     url='https://www.github.com/monero-ecosystem/python-monerorpc',
-    download_url='https://github.com/monero-ecosystem/python-monerorpc/archive/0.5.3.tar.gz',
+    download_url='https://github.com/monero-ecosystem/python-monerorpc/archive/0.5.4.tar.gz',
     packages=['monerorpc'],
     install_requires=[
         'requests',
