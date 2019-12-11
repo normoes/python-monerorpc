4d65c7d77e932e7ecd477a43b18a334e79a489e4
Norman Moeschter <norman.moeschter@gmail.com>
2019-12-11 19:10:26 +0000

    Add username and password arguments.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from _version import __version__
 
 setup(
     name="python-monerorpc",
-    version="0.5.5",
+    version=__version__,
     description="Enhanced version of python-jsonrpc for Monero (monerod, monero-wallet-rpc).",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -13,9 +14,9 @@ setup(
     maintainer="Norman Moeschter-Schenck",
     maintainer_email="<norman.moeschter@gmail.com>",
     url="https://www.github.com/monero-ecosystem/python-monerorpc",
-    download_url="https://github.com/monero-ecosystem/python-monerorpc/archive/0.5.5.tar.gz",
+    download_url=f"https://github.com/monero-ecosystem/python-monerorpc/archive/{__version__}.tar.gz",
     packages=["monerorpc"],
-    install_requires=["requests"],
+    install_requires=["requests>=2.22.0"],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
